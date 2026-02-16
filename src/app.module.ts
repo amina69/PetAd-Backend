0277473f6bd5d15cfcbf0c861d4ee46c19a45c51
samad13 <yusufabdulsamad93@gmail.com>
2026-02-16 15:11:18 +0100

    initial backend scaffold

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PetsModule } from './pets/pets.module';
+import { AdoptionModule } from './adoption/adoption.module';
+import { CustodyModule } from './custody/custody.module';
+import { EscrowModule } from './escrow/escrow.module';
+import { EventsModule } from './events/events.module';
+import { StellarModule } from './stellar/stellar.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [PetsModule, AdoptionModule, CustodyModule, EscrowModule, EventsModule, StellarModule, AuthModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
